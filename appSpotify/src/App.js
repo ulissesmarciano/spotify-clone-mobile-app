8901725b5b8d719284dbafdfc727e3715d11660a
Ulisses Marciano Gonçalves <104742158+ulissesmarciano@users.noreply.github.com>
2022-08-17 15:43:30 -0300

    instalação das libs para o "bar navigation"

--- appSpotify/src/App.js
+++ appSpotify/src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
-import Home from "./pages/home/Home.js"
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 
-const App = () => {
+import Home from "./pages/home/Home.js";
+import Library from "./pages/library/Library.js"
+import Search from "./pages/search/Search.js"
+
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
     return (
-        <Home />
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen name="Home" component={Home}/> 
+                <Stack.Screen name="Search" component={Search}/>
+                <Stack.Screen name="Library" component={Library}/>             
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 };
 
-export default App;
---
+++ appSpotify/src/pages/search/Search.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Search = () => {
+    <View>
+        <Text>Biblioteca</Text>
+    </View>
+};
+
+export default Search;
