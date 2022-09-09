76edee3135a66a89e23153fb2cc7a6cdefacc9db
Ulisses Marciano Gonçalves <104742158+ulissesmarciano@users.noreply.github.com>
2022-09-09 14:56:53 -0300

    Revisão da seção 'variousselection' na página Explorer.

--- appSpotify/src/pages/search/components/variousselection/styled.js
+++ appSpotify/src/pages/search/components/variousselection/styled.js
@@ -9,65 +9,62 @@ export default StyleSheet.create({
         marginLeft: 15,
     },
 
-    imageContainer:{
+    itemContainer:{
         marginTop: 15,
-        marginHorizontal: 15,
+        marginHorizontal: 7.5,
         flexDirection: 'row',
         justifyContent: 'space-between',
     },
 
     item: {
-        width: 175,
         height: 100,
     },
 
     item1: {
-        width: 175,
         height: 100,
         backgroundColor: '#rgb(240, 172, 0)',
     },
 
     item2: {
-        width: 175,
         height: 100,
         backgroundColor: '#rgb(60, 147, 26)',
     },
 
     item3: {
-        width: 175,
         height: 100,
         backgroundColor: '#rgb(0, 150, 204)',
     },
 
     item4: {
-        width: 175,
         height: 100,
         backgroundColor: '#rgb(204, 0, 204)',
     },
 
     item5: {
-        width: 175,
         height: 100,
         backgroundColor: '#rgb(145, 0, 235)',
     },
 
     item6: {
-        width: 175,
         height: 100,
         backgroundColor: '#rgb(158, 0, 15)',
     },
 
     item7: {
-        width: 175,
         height: 100,
         backgroundColor: '#rgb(240, 172, 0)',
     },
 
     item8: {
-        width: 175,
         height: 100,
         backgroundColor: '#rgb(0, 34, 255)',
     },
+
+    imageContainer: {
+        flex: 1,
+        marginHorizontal: 7.5,
+    },
+
 
     textItem: {
         color: 'white',
--- appSpotify/src/pages/search/components/variousselection/variousselection.js
+++ appSpotify/src/pages/search/components/variousselection/variousselection.js
@@ -1,95 +1,123 @@
 import React from 'react';
-import { View, Text, ImageBackground } from 'react-native';
+import { View, Text, ImageBackground, TouchableOpacity } from 'react-native';
 
-import S from './styled.js'
+import { useNavigation } from '@react-navigation/native';
+
+import S from './styled.js';
+
+import PopularReleasesImage from './backimages/podcasts.png';
+import ForYouImage from './backimages/feitoparavoce.png';
+import OtherImage from './backimages/outros.png';
 
 const VariousSection = () => {
+
+    const navigator = useNavigation();
+
     return (
-        
         <View>
             <View>
                 <Text style={S.title}>Navegar por todas as seções</Text>
             </View>
-            <View style={S.imageContainer}>
-                <ImageBackground 
-                style={S.item}
-                imageStyle={{ borderRadius: 4}} 
-                source={require('./backimages/podcasts.png')}
-                >
-                    <Text style={S.textItem}>Podcasts</Text>
-                </ImageBackground>
-                <ImageBackground 
-                style={S.item} 
-                imageStyle={{ borderRadius: 4}} 
-                source={require('./backimages/feitoparavoce.png')}
-                >
-                    <Text style={S.textItem}>Feito para{'\n'}você</Text>
-                </ImageBackground>
+            <View style={S.itemContainer}>
+                <TouchableOpacity style={S.imageContainer} onPress={() => navigator.navigate("Podcast Screen")}>
+                    <ImageBackground 
+                    style={S.item}
+                    imageStyle={{ borderRadius: 4}} 
+                    source={PopularReleasesImage}
+                    >
+                        <Text style={S.textItem}>Podcasts</Text>
+                    </ImageBackground>
+                </TouchableOpacity>
+                <TouchableOpacity style={S.imageContainer} onPress={() => navigator.navigate("Podcast Screen")}>
+                    <ImageBackground 
+                    style={S.item} 
+                    imageStyle={{ borderRadius: 4}} 
+                    source={ForYouImage}
+                    >
+                        <Text style={S.textItem}>Feito para{'\n'}você</Text>
+                    </ImageBackground>
+                </TouchableOpacity>
             </View>
-            <View style={S.imageContainer}>
-                <ImageBackground 
-                style={S.item1}
-                imageStyle={{ borderRadius: 4}} 
-                source={require('./backimages/outros.png')}
-                >
-                    <Text style={S.textItem}>Outros...</Text>
-                </ImageBackground>
-                <ImageBackground 
-                style={S.item2} 
-                imageStyle={{ borderRadius: 4}} 
-                source={require('./backimages/outros.png')}
-                >
-                    <Text style={S.textItem}>Outros...</Text>
-                </ImageBackground>
+            <View style={S.itemContainer}>
+                <TouchableOpacity style={S.imageContainer} onPress={() => navigator.navigate("Podcast Screen")}>
+                    <ImageBackground 
+                    style={S.item1}
+                    imageStyle={{ borderRadius: 4}} 
+                    source={OtherImage}
+                    >
+                        <Text style={S.textItem}>Outros...</Text>
+                    </ImageBackground>
+                </TouchableOpacity>
+                <TouchableOpacity style={S.imageContainer} onPress={() => navigator.navigate("Playlist Screen")}>
+                    <ImageBackground 
+                    style={S.item2} 
+                    imageStyle={{ borderRadius: 4}} 
+                    source={OtherImage}
+                    >
+                        <Text style={S.textItem}>Outros...</Text>
+                    </ImageBackground>
+                </TouchableOpacity>
             </View>
-            <View style={S.imageContainer}>
-                <ImageBackground 
-                style={S.item3}
-                imageStyle={{ borderRadius: 4}} 
-                source={require('./backimages/outros.png')}
-                >
-                    <Text style={S.textItem}>Outros...</Text>
-                </ImageBackground>
-                <ImageBackground 
-                style={S.item4} 
-                imageStyle={{ borderRadius: 4}} 
-                source={require('./backimages/outros.png')}
-                >
-                    <Text style={S.textItem}>Outros...</Text>
-                </ImageBackground>
+            <View style={S.itemContainer}>
+                <TouchableOpacity style={S.imageContainer} onPress={() => navigator.navigate("Podcast Screen")}>
+                    <ImageBackground 
+                    style={S.item3}
+                    imageStyle={{ borderRadius: 4}} 
+                    source={OtherImage}
+                    >
+                        <Text style={S.textItem}>Outros...</Text>
+                    </ImageBackground>
+                </TouchableOpacity>
+                <TouchableOpacity style={S.imageContainer} onPress={() => navigator.navigate("Playlist Screen")}>
+                    <ImageBackground 
+                    style={S.item4} 
+                    imageStyle={{ borderRadius: 4}} 
+                    source={OtherImage}
+                    >
+                        <Text style={S.textItem}>Outros...</Text>
+                    </ImageBackground>
+                </TouchableOpacity>
             </View>
-            <View style={S.imageContainer}>
-                <ImageBackground 
-                style={S.item5}
-                imageStyle={{ borderRadius: 4}} 
-                source={require('./backimages/outros.png')}
-                >
-                    <Text style={S.textItem}>Outros...</Text>
-                </ImageBackground>
-                <ImageBackground 
-                style={S.item6} 
-                imageStyle={{ borderRadius: 4}} 
-                source={require('./backimages/outros.png')}
-                >
-                    <Text style={S.textItem}>Outros...</Text>
-                </ImageBackground>
+            <View style={S.itemContainer}>
+                <TouchableOpacity style={S.imageContainer} onPress={() => navigator.navigate("Podcast Screen")}>
+                    <ImageBackground 
+                    style={S.item5}
+                    imageStyle={{ borderRadius: 4}} 
+                    source={OtherImage}
+                    >
+                        <Text style={S.textItem}>Outros...</Text>
+                    </ImageBackground>
+                </TouchableOpacity>
+                <TouchableOpacity style={S.imageContainer} onPress={() => navigator.navigate("Playlist Screen")}>
+                    <ImageBackground 
+                    style={S.item6} 
+                    imageStyle={{ borderRadius: 4}} 
+                    source={OtherImage}
+                    >
+                        <Text style={S.textItem}>Outros...</Text>
+                    </ImageBackground>
+                </TouchableOpacity>
             </View>
-            <View style={S.imageContainer}>
-                <ImageBackground 
-                style={S.item7}
-                imageStyle={{ borderRadius: 4}} 
-                source={require('./backimages/outros.png')}
-                >
-                    <Text style={S.textItem}>Outros...</Text>
-                </ImageBackground>
-                <ImageBackground 
-                style={S.item8} 
-                imageStyle={{ borderRadius: 4}} 
-                source={require('./backimages/outros.png')}
-                >
-                    <Text style={S.textItem}>Outros...</Text>
-                </ImageBackground>
-            </View>            
+            <View style={S.itemContainer}>
+                <TouchableOpacity style={S.imageContainer} onPress={() => navigator.navigate("Podcast Screen")}>
+                    <ImageBackground 
+                    style={S.item7}
+                    imageStyle={{ borderRadius: 4}} 
+                    source={OtherImage}
+                    >
+                        <Text style={S.textItem}>Outros...</Text>
+                    </ImageBackground>
+                </TouchableOpacity>
+                <TouchableOpacity style={S.imageContainer} onPress={() => navigator.navigate("Playlist Screen")}>
+                    <ImageBackground 
+                    style={S.item8} 
+                    imageStyle={{ borderRadius: 4}} 
+                    source={OtherImage}
+                    >
+                        <Text style={S.textItem}>Outros...</Text>
+                    </ImageBackground>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 };
